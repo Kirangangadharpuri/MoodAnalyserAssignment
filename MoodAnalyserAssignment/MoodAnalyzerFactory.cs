3b6ce3e08c4215bf3a29dd49f3dbb2466f6d973c
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-11 20:13:03 -1200

    Mood Analyser UC5

--- MoodAnalyserAssignment/MoodAnalyzerFactory.cs
+++ MoodAnalyserAssignment/MoodAnalyzerFactory.cs
@@ -10,6 +10,32 @@ namespace MoodAnalyserAssignment
 {
     public class MoodAnalyzerFactory
     {
+        //UC 5 : For parameterised constructor by passing message parameter to the class method.
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName)
+        {
+            Type type = typeof(MoodAnalyser);
+            if(type.Name.Equals(className)|| type.FullName.Equals(className))
+            {
+                if(type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { "HAPPY" });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, "Constructor is not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, "Class is not Found");
+
+            }
+        }
+
+
+        //UC 4 : CreateMethodAnalyse method to create object of MoodAnalyse class.
         public static object CreateMoodAnalyse(string className, string constructorName)
         {
             string pattern = @"." + constructorName + "$";
